e9891484ff0ee1895af22280bef55734c2ee272c
NeuTrix <neutrix@brandgeneering.com>
2018-03-01 15:21:33 -0800

    restore user model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,82 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-env node, mocha */
+let express = require('express');
+let router = express.Router();
+let User = require('../models/user.model');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// ========== * READ a list of all users
+router.get('/', (req, res) => {
+	// res.send('the GET/ rte');
+	User.find({ },(err, users) => {
+		if(err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(users);
+		}
+	});
+});
+
+// ========= * CREATE a new user item
+router.post('/', (req, res) => {
+
+	let _user = new User(req.body);
+
+	_user.save((err, user) => {
+		if(err) {
+			res.status(500).send(err);
+		} else {
+			res.status(201).send(user);
+		}
+	});
+});
+
+// ========= * READ a specific user item
+router.get('/:id', (req, res) => {
+
+	User.findById(
+		req.params.id, 
+		(err, user) => {
+			if(err) {
+				res.status(500).send(err);
+			} else {
+				res.status(200).send(user);
+			}
+	});
+});
+
+// ========= * UPDATE a specific item
+router.put ('/:id', (req, res) => {
+
+		User.findByIdAndUpdate (
+			req.params.id, 
+			req.body, 
+			{ new: true }, 
+			(err, user ) => {
+
+			if(err) {
+				res.status(500).send(err);
+			}
+				res.status(200).send(
+					user);
+		});
+	}
+);
+
+// ========= * DELETE a specific item
+router.delete('/:id', (req, res) => {
+
+	User.findByIdAndRemove(req.params.id, (error, user) => {
+
+		if(error) {
+			return res.status(500).send(err)
+		} else {
+			let message = {
+				text: `The user with id ${user._id} has been deleted`
+			}
+			console.log("***it's gone")
+			return res.status(200).send(message.text);
+		}
+	});
+
 });
 
 module.exports = router;
