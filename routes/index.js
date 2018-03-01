2309f5bc2fb9f7399751ecf69649beae8a47f1ee
NeuTrix <neutrix@brandgeneering.com>
2018-03-01 14:19:16 -0800

    build get and post register routes"

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,30 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 	// res.render('index', { title: 'Express' });
-	res.send('<h1>Todos API  ==> under construction</h1>');
+	// res.send('<h1>Todos API  ==> under construction</h1>');
+	res.render('index', { user: req.user });
 });
+
+router.get('/register', function(req, res) {
+	res.render('register', { });
+})
+
+router.post('/register', function(req, res) {
+	Account.register(
+		new Account({username: req.body.username }),
+		req.body.password,
+		function(err, account) {
+			if (err) {
+				return res.render('register', { account: account });
+			}
+			passport.authenticate('local')(req, res, function() {
+				res.redirect('/');
+			});
+	});
+});
+
+
 
 module.exports = router;
