a8eeb952f025fa7e6c14dbf9e4c6486d2df0cd22
NeuTrix <neutrix@brandgeneering.com>
2018-03-01 12:33:35 -0800

    cp user file

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a Schema (alpha sort props)
+var TodoSchema = new Schema({
+	completed: {
+		type: Boolean,
+		default: false
+	},
+	date: { type: Date, default: '1936-05-24' },
+	details: { type: String, default: "Black Panther of Wakanda" },
+	owner: { type: String, default: "Prince T'Challa" },
+	rank: { type: String, default: "Med" },
+	task: {
+		type: String,
+		default: "Making the world better",
+		required: [ 
+		true, "Please add a Task" ]
+	},
+});
+
+// create a Model
+module.exports = mongoose.models.Todos || mongoose.model('Todos', TodoSchema);
