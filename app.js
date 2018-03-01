f3198969953f19ffe1eaae4cb46b0c57f6182269
NeuTrix <neutrix@brandgeneering.com>
2018-03-01 13:08:31 -0800

    update app.js to tutorial std

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 /* eslint-env node, es6, JSX */
-let express = require('express');
-let cors = require('cors');
-
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let cors = require('cors');
+let express = require('express');
+let favicon = require('serve-favicon');
+let LocalStrategy = require('passport-local').Strategy
+let logger = require('morgan');
+let mongoose = require('mongoose');
+let passport = require('passport')
+let path = require('path');
+
+// +++++++++ Routes  +++++++++ 
 
 let index = require('./routes/index');
-let todos = require('./routes/todos.routes');
+let todos = require('./routes/todos');
+let users = require('./routes/users');
+
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// =========================== Connect database
-let mongoose = require('mongoose');
+// +++++++++ mongoose  +++++++++ 
 
-// ========= test db
+// +++++++++  test db 
 // let mongoDB = 'mongodb://Tester:test2015@ds239117.mlab.com:39117/todo-test-db';
 
-// ========= default db
+// +++++++++ default db  
 let mongoDB = 'mongodb://Tester:test2015@ds135537.mlab.com:35537/react-redux-todo';
 
 // establish pending connection to db
@@ -46,11 +51,30 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// ??? passport tutorial 
+app.use(require('express-session')({
+	secret: 'monkey ball',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', index);
 app.use('/api/todos', todos);
+app.use('/api/users', users);
+
+// +++++++++ passport config  +++++++++ 
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
+// +++++++++ error handling  +++++++++ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,8 +83,28 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+// +++++++++ dev error handler to print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req,res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		})
+	})
+}
+
+// +++++++++ production erro handler: no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// +++++++++ prior handler  +++++++++ 
+/*app.use(function(err, req, res, next) {
 // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -68,6 +112,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 module.exports = app;
