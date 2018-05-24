8facfd474a5c21b8d943fcaeb26061059f02713e
NeuTrix <neutrix@brandgeneering.com>
2018-05-24 09:44:57 -0700

    tweak

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -2,10 +2,10 @@
 const dbConnection = 
 
 // local db connection
-// 'mongodb://localhost/test'
+'mongodb://localhost/test_todo';
 
 // default db  
-'mongodb://Tester:test2015@ds135537.mlab.com:35537/react-redux-todo';
+// 'mongodb://Tester:test2015@ds135537.mlab.com:35537/react-redux-todo';
 
 // test db 
 // 'mongodb://Tester:test2015@ds239117.mlab.com:39117/todo-test-db';
